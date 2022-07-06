508b43782855c925e796da722346669287d39215
Jae Han Song <rfv1479@gmail.com>
2022-07-06 20:31:14 +0900

    Feat : add the docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,18 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'JaeHan TIL',
+  tagline: 'TIL is the best choice',
+  url: 'https://wisesaturn.github.io',
+  baseUrl: '/TIL/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'wisesaturn', // Usually your GitHub org/user name.
+  projectName: 'TIL', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -129,11 +129,4 @@ const config = {
     }),
 };
 
-module.exports = {
-  // ...
-  url: 'https://wisesaturn.github.io', // GitHub organization 또는 username + github.io
-  baseUrl: '/TIL/',
-  projectName: 'TIL',
-  organizationName: 'wisesaturn', // GitHub organization 또는 username
-  // ...
-};
+module.exports = config;
